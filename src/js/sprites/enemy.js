ec4194bfcc1a68e65ce9923db18343b0346c183b
Reik Val <guardian_de_las_sombras@hotmail.com>
2015-12-12 20:59:16 +0100

    Fixed slime animation

--- src/js/sprites/enemy.js
+++ src/js/sprites/enemy.js
@@ -7,6 +7,7 @@ export class Enemy extends Phaser.Sprite {
         this.game.physics.arcade.enable(this);
 
         this.body.velocity.x = 50;
+        this.scale.setTo(-1, 1);
 
         this.animations.add("patrol", [0, 1], 4, true);
         this.play("patrol");
@@ -18,11 +19,13 @@ export class Enemy extends Phaser.Sprite {
                 let rightTile = this.gameState.map.getTileWorldXY(this.body.right, this.body.bottom + 1, 32, 32, this.gameState.mapLayer);
                 if (rightTile && rightTile.index === 1) {
                     this.body.velocity.x *= -1;
+                    this.scale.setTo(1, 1);
                 }
             } else {
                 let leftTile = this.gameState.map.getTileWorldXY(this.body.position.x, this.body.bottom + 1, 32, 32, this.gameState.mapLayer);
                 if (leftTile && leftTile.index === 1) {
                     this.body.velocity.x *= -1;
+                    this.scale.setTo(-1, 1);
                 }
             }
         }
