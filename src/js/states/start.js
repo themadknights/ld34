0a5101760aaa991cf9a4174d61aab9f5d391e37e
Reik Val <guardian_de_las_sombras@hotmail.com>
2015-12-12 15:23:05 +0100

    Added timer for cast spells

--- src/js/states/start.js
+++ src/js/states/start.js
@@ -16,6 +16,34 @@ export class StartState extends Phaser.State {
 
         // Create player
         this.player = new Player(this, 200, 200);
+
+        //Adding keyboard
+        this.zKey = this.game.input.keyboard.addKey(Phaser.KeyCode.Z);
+        this.xKey = this.game.input.keyboard.addKey(Phaser.KeyCode.X);
+        //Add timer
+        this.spellTimer = this.game.time.create(false);
+        this.spellTimer.start();
+        this.spell = "";
+        this.zKey.onDown.add(function() {
+            this.spell += "Z";
+            this.spellTimer.stop();
+            this.spellTimer.add(Phaser.Timer.SECOND, function() {
+                //TODO: Cast
+                console.log(this.spell);
+                this.spell = "";
+            }, this);
+            this.spellTimer.start();
+        }, this);
+        this.xKey.onDown.add(function() {
+            this.spell += "X";
+            this.spellTimer.stop();
+            this.spellTimer.add(Phaser.Timer.SECOND, function() {
+                //TODO: Cast
+                console.log(this.spell);
+                this.spell = "";
+            }, this);
+            this.spellTimer.start();
+        }, this);
     }
 
     update() {
