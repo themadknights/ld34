4430acee08fe24489e698e217760405a1178c00c
beagleknight <david.morcillo@gmail.com>
2015-12-12 14:58:02 +0100

    Load a simple tilemap and check collision with player

--- src/js/states/load.js
+++ src/js/states/load.js
@@ -7,6 +7,8 @@ export class LoadState extends Phaser.State {
         this.loadingText = this.game.add.bitmapText(this.game.world.centerX, this.game.world.centerY, 'carrier_command', "Loading...", 18);
         this.loadingText.anchor.setTo(0.5);
         this.load.image('player', 'images/player.png');
+        this.load.image('tileset', 'images/tileset.png');
+        this.load.tilemap('level1', 'json/level1.json', null, Phaser.Tilemap.TILED_JSON);
     }
 
     create () {
--- src/js/states/start.js
+++ src/js/states/start.js
@@ -14,12 +14,35 @@ export class StartState extends Phaser.State {
         //Creating gravity
         this.physics.arcade.gravity.y = 300;
 
+        // Create world
+        this.map = this.add.tilemap('level1');
+        this.map.addTilesetImage('tileset');
+        this.map.setCollision(2);
+        this.mapLayer = this.map.createLayer('foreground');
+        this.mapLayer.resizeWorld();
+
         // Create player
-        this.player = new Player(this, 200, 200);
+        this.player = new Player(this, 100, 100);
+
+        // TODO: for debugging purposes
+        this.cursors = this.game.input.keyboard.createCursorKeys();
     }
 
     update() {
+        this.game.physics.arcade.collide(this.player, this.mapLayer);
 
+        // TODO: for debugging purposes
+        if (this.cursors.up.isDown) {
+            this.player.body.velocity.y = -200;
+        }
+        
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -200;
+        } else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = 200;
+        } else {
+            this.player.body.velocity.x = 0;
+        }
     }
 
     render() {
