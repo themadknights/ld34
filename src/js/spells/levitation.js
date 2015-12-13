7985d42429aef57de21406ea7124c8cd029e871e
beagleknight <david.morcillo@gmail.com>
2015-12-13 18:44:50 +0100

    Add bounce effect to levitation spell

--- src/js/spells/levitation.js
+++ src/js/spells/levitation.js
@@ -9,9 +9,19 @@ export class LevitationSpell extends Spell {
     castAction() {
         this.user.body.allowGravity = false;
         this.user.body.velocity.y = 0;
+
+        this.levitationTween = this.user.game.add.tween(this.user);
+
+        this.levitationTween.to({
+            y: "-10"
+        }, 200, Phaser.Easing.Bounce.InOut, true, 0, 4, true);
+
+        this.levitationTween.loop(true);
+        this.levitationTween.start();
     }
 
     cancelAction() {
         this.user.body.allowGravity = true;
+        this.levitationTween.stop();
     }
 }
